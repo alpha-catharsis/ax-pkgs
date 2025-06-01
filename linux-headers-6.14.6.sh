39e1c9abdae5031ce2735ab0b3a3a573cbdc7611
Alpha Catharsis <alpha.catharsis@gmail.com>
2025-06-01 21:53:46 +0200

    Worked on linux-headers

--- linux-headers-6.14.6.sh
+++ linux-headers-6.14.6.sh
@@ -2,13 +2,23 @@ pkg_name="linux-headers"
 pkg_ver="6.14.6"
 
 function pkg_fetch {
-    true
+    local url="https://www.kernel.org/pub/linux/kernel/v6.x/linux-6.14.6.tar.xz"
+    fetch_url "${url}" "archive.tar.xz"
 }
 
 function pkg_prepare {
-    true
+    local cmd=(tar -xf "archive.tar.xz" --strip-components=1)
+    entry "Cleaning up package"
+    local cmd=(make mrproper)
+    shell_cmd "${cmd[@]}"
+    entry "Generating headers"
+    cmd=(make headers)
+    shell_cmd "${cmd[@]}"
 }
 
 function pkg_install {
-    true
+    local inst_dir="${AX_INSTS}/${pkg_name}-${pkg_ver}"
+    create_dirs "${inst_dir}/usr"
+    cmd=(cp -r usr/include "${inst_dir}/usr")
+    shell_cmd "${cmd[@]}"
 }
